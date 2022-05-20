88bb76b6324d554dcec331ada888169e9df4ad10
Yuji_Itadorii <rawat141abhay@gmail.com>
2022-05-20 21:15:48 +0530

    Making sorting algo

--- DSA-Code/Lab Assignment/Experiment-3/5.c
+++ DSA-Code/Lab Assignment/Experiment-3/5.c
@@ -5,65 +5,61 @@ struct student
     int student_name, student_roll_no, total_marks;
 };
 
-void merge(struct student arr[], int l, int m, int r)
-{
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
+void merge(struct student arr[] , int l , int m , int r){
+    int n1 = m-l+1 , n2 = r-m;
+    int left[n1] , right[n2];
+    for( int i = 0 ; i<n1 ; i++){
+        left[i] = arr[l+i].student_roll_no;
+    }
 
-    struct student L[n1], R[n2];
-
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
+    for(int  i = 0 ; i<n2 ; i++){
+        right[i] = arr[m+1+i].student_roll_no;
+    }
+    int i =0 , j=0 , k=l;
+    while (i<n1 && j <n2)
     {
-        
-        if (L[i].student_roll_no <= R[j].student_roll_no)
+        if (left[i]<=right[j])
         {
-            arr[k] = L[i];
+            count++;
+            arr[k].student_roll_no = left[i];
             i++;
+            k++;
         }
         else
         {
-            arr[k] = R[j];
+            count++;
+            arr[k].student_roll_no = right[j];
             j++;
+            k++;
         }
-        k++;
-        count++;
     }
 
-    while (i < n1)
+    while (i<n1)
     {
-        arr[k] = L[i];
+        arr[k].student_roll_no = left[i];
         i++;
         k++;
     }
 
-    while (j < n2)
+    while (j<n2)
     {
-        arr[k] = R[j];
+        arr[k].student_roll_no = right[j];
         j++;
         k++;
     }
+    
+    
 }
 
-void mergeSort(struct student arr[], int l, int r)
-{
-    if (l < r)
+void mergeSort(struct student arr[] , int l ,int r){
+    if (l<r)
     {
-        int m = l + (r - l) / 2;
-
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        merge(arr, l, m, r);
+        int m = l + (r-l)/2;
+        mergeSort(arr , l , m);
+        mergeSort(arr , m+1 , r);
+        merge(arr, l ,m ,r);
     }
+    
 }
 
 void dispaly(int size, struct student arr[size])
