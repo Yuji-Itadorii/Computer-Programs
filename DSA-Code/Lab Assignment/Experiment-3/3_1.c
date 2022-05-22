72cb55e231e0a8f8fee2382875ad95cd0b46886a
Yuji_Itadorii <rawat141abhay@gmail.com>
2022-05-22 17:22:55 +0530

    enhancing

--- DSA-Code/Lab Assignment/Experiment-3/3_1.c
+++ DSA-Code/Lab Assignment/Experiment-3/3_1.c
@@ -3,6 +3,7 @@ int count ;
 void insertionSort(int arr[] , int size ){
     for (int i = 1; i < size; i++)
     {
+            count++;
         int temp = arr[i];
         int j = i-1;
         while (j>=0 && arr[j]>temp)
