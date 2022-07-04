5c2e6fb46f879aa9be55f9ebf9b98d2742116f9b
Yuji_Itadorii <rawat141abhay@gmail.com>
2022-07-04 20:51:25 +0530

    Modifying insertion Sort

--- DSA-Code/Standard-Code/Sorting/insertionSort.cpp
+++ DSA-Code/Standard-Code/Sorting/insertionSort.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void mergeSort(int arr[], int size)
+void insertionSort(int arr[], int size)
 {
     for (int i = 0; i < size; i++)
     {
@@ -17,7 +17,7 @@ void mergeSort(int arr[], int size)
 int main()
 {
     int arr[] = {3, 4, 2, 8, 5};
-    mergeSort(arr, 5);
+    insertionSort(arr, 5);
     for (int i = 0; i < 5; i++)
         cout << arr[i] << " ";
     return 0;
