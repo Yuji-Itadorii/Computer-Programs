dcde84770422855e0b12a4dd46b145d854cd6959
Yuji_Itadorii <rawat141abhay@gmail.com>
2023-08-08 17:34:10 +0530

    Dated 8-August-2023

---
+++ Important Questions/HashMap/Fractions_with_pair_1.cpp
@@ -0,0 +1,54 @@
+// https://practice.geeksforgeeks.org/problems/fraction-pairs-with-sum-1/1
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    int countFractions(int n, int numerator[], int denominator[])
+    {
+        map<pair<int, int>, int> m;
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int gcd = __gcd(numerator[i], denominator[i]);
+
+            int x = numerator[i] / gcd;
+            int y = denominator[i] / gcd;
+
+            count += m[{y - x, y}];
+
+            m[{x, y}]++;
+        }
+
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int numerator[n], denominator[n];
+        for (int i = 0; i < n; i++)
+            cin >> numerator[i];
+        for (int i = 0; i < n; i++)
+            cin >> denominator[i];
+        Solution ob;
+        int ans = ob.countFractions(n, numerator, denominator);
+        cout << ans << endl;
+    }
+}
+// } Driver Code Ends
