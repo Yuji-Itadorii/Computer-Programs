e0b83a44a5c1497e60fda14958500b60d566b713
Yuji_Itadorii <rawat141abhay@gmail.com>
2023-09-07 11:16:21 +0530

    Update: 2023-09-07 11:16:21

---
+++ LeetCode Questions/92_Reverse_LinkedList_II.cpp
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/reverse-linked-list-ii/description/?envType=daily-question&envId=2023-09-07
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+//  Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseBetween(ListNode *head, int left, int right)
+    {
+        ListNode *dummy = new ListNode(0); // created dummy node
+        dummy->next = head;
+        ListNode *prev = dummy; // intialising prev pointer on dummy node
+
+        for (int i = 0; i < left - 1; i++)
+            prev = prev->next; // adjusting the prev pointer on it's actual index
+
+        ListNode *curr = prev->next; // curr pointer will be just after prev
+        // reversing
+        for (int i = 0; i < right - left; i++)
+        {
+            ListNode *forw = curr->next; // forw pointer will be after curr
+            curr->next = forw->next;
+            forw->next = prev->next;
+            prev->next = forw;
+        }
+        return dummy->next;
+    }
+};
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+    }
+    return 0;
+}
